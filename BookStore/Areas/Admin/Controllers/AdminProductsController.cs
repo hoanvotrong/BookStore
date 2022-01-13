58ee00542803b8073a9d85d0bcf37554391b5a6b
ThuysNga <19521881@gm.uit.edu.vn>
2022-01-13 22:12:47 +0700

    edit css

--- BookStore/Areas/Admin/Controllers/AdminProductsController.cs
+++ BookStore/Areas/Admin/Controllers/AdminProductsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BookStore.Models;
+using PagedList.Core;
 
 namespace BookStore.Areas.Admin.Controllers
 {
